90c100f9ad7ad62a47f844dcdc16c26026f6ac30
Felipe Gonçalves Assis <felipegassis@gmail.com>
2015-07-18 23:08:07 -0300

    Make commit mapping interface symmetric.

---
+++ utils/map_lookup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Lookup commit corresponding to the one given in the map.
+
+cat ../map/${1:0:2}/${1:2}
+ret=$?
+
+if ! [ $ret -eq 0 ]; then
+  echo 'Could not retrieve commit mirroring $1.'
+fi
+
+exit $ret
+
