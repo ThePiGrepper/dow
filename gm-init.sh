425cb7cad2e96ae6bb96bc4ff84f42ffc2efbc1e
Felipe Gonçalves Assis <felipegassis@gmail.com>
2015-07-19 15:25:06 -0300

    Factor out CVS update functionality.
    Still a mock implementation.

--- gm-init.sh
+++ gm-init.sh
@@ -7,6 +7,7 @@ git init data
 mkdir map
 
 ln -s gm_draft/utils .
+ln -s gm_draft/$1_cmd cvs_cmd
 
 cd versions/.git/hooks
 ln -s ../../../gm_draft/hooks/versions/pre-commit .
---
+++ mock_cmd/co.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Copy contents of file $1, version $2, into data repository.
+
+file=$1
+version=$2
+data_repo=../data
+
+cd "$data_repo"
+mkdir -p $(dirname $file)
+echo "$file;$version" > $data_repo/$file
+
